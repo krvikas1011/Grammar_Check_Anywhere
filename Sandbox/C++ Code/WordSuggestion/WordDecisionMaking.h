f7566e761e7b607d9299525e9c045952a5d0fe2d
WHQ_NT_DOMAIN\AK045801 <arshia.khan@cerner.com>
2018-12-04 15:09:07 +0530

    Removed duplicate code

--- Sandbox/C++ Code/WordSuggestion/WordDecisionMaking.h
+++ Sandbox/C++ Code/WordSuggestion/WordDecisionMaking.h
@@ -9,15 +9,6 @@
 #include<set>
 using namespace std;
 
-void checkIfWordExistAndAdd(set<string> wordSet, string sInput);
-bool matchesSubString(string strExistingWord, string sInput);
-bool isSubString(string subStr, string sInput);
-int getUserChoice();
-void addToList(int iUserChoice, string sInput, map<int, string>mapOfSuggestedWords, set<string>& wordSet);
-void addToList(string sInput, set<string>& wordSet);
-void createMapOfSuggestedWords(map<int, string>& mapOfSuggestedWords, set<string> wordSet, string sInput);
-
-
 class CWordSuggestionProcessor
 {
 public :
